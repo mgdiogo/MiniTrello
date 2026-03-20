2b8a7d63ea96eff44d05764533703d8ecad1d33a
mgdiogo <miguel.g.diogo@gmail.com>
2026-03-20 01:59:21 +0000

    🚧 -> api call to login
    - Simple API call to /auth/login working

--- minitrello-frontend/src/pages/Home.tsx
+++ minitrello-frontend/src/pages/Home.tsx
@@ -1,34 +1,52 @@
-import { useLayoutEffect } from "react"
+import { useLayoutEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
+
 
 export default function HomePage() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     useLayoutEffect(() => {
         document.title = "MiniTrello - Start Organizing Yourself!"
     }, [])
+
+    async function handleSubmit(e: React.SubmitEvent) {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("/api/auth/login", { email, password });
+            
+            console.log(response.data);
+        } catch (error) {
+            if (axios.isAxiosError(error))
+                console.error("Login failed:", error.response?.data.message || error.message);
+        }
+    }
 
     return (
         <main>
             <div className="login-page">
                 <div className="login-container">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <h1>Login</h1>
                         <div className="form-group">
-                            <input className="form-input" type="email" placeholder="Email" required />
+                            <input className="form-input" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                         </div>
                         <div className="form-group">
-                            <input className="form-input" type="password" placeholder="Password" required />
+                            <input className="form-input" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        </div>
+                        <div className="forgot-password">
+                            <Link className="a" to="#">
+                                Forgot Password?
+                            </Link>
+                        </div>
+                        <div className="button-submit">
+                            <button className="button-primary" type="submit">
+                                Login
+                            </button>
                         </div>
                     </form>
-                    <div className="forgot-password">
-                        <Link className="a" to="#">
-                            Forgot Password?
-                        </Link>
-                    </div>
-                    <div className="button-submit">
-                        <button className="button-primary"type="submit">
-                            Login
-                        </button>
-                    </div>
                 </div>
             </div>
         </main>
